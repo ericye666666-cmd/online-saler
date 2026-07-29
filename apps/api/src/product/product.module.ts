9b85127098e2127063ecd9339f0c62de88ffc6b3
ericye666666-cmd <ericye666666@gmail.com>
2026-07-29 13:13:08 +0300

    feat(api): register formal barcode API

--- apps/api/src/product/product.module.ts
+++ apps/api/src/product/product.module.ts
@@ -1,5 +1,7 @@
 import { Module } from "@nestjs/common";
 import { ProductApplicationService } from "./product-application.service";
+import { ProductBarcodeController } from "./product-barcode.controller";
+import { ProductBarcodeService } from "./product-barcode.service";
 import { ProductCalibrationController } from "./product-calibration.controller";
 import { ProductCalibrationService } from "./product-calibration.service";
 import { PRODUCT_REPOSITORY } from "./product.repository";
@@ -7,10 +9,11 @@ import { PrismaProductRepository } from "./prisma-product.repository";
 import { ProductStateMachine } from "./product-state-machine";
 
 @Module({
-  controllers: [ProductCalibrationController],
+  controllers: [ProductCalibrationController, ProductBarcodeController],
   providers: [
     ProductApplicationService,
     ProductCalibrationService,
+    ProductBarcodeService,
     ProductStateMachine,
     {
       provide: PRODUCT_REPOSITORY,
